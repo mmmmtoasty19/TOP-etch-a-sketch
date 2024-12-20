90f9439d67f866dcccf8491f17d2d373fa0d3111
Kyle Belanger <Kyleb44@hotmail.com>
2024-12-20 08:02:25 -0500

    add hover feature

--- js/javascript.js
+++ js/javascript.js
@@ -1,20 +1,35 @@
 const container = document.querySelector(".container")
 
-let gridNum = 16;
+// let gridNum = 16;
 
 
-function createGrid () {
+function createGrid (gridNum) {
   for (let i = 0; i < gridNum; i++){
     const row = document.createElement("div");
     row.classList.add("row");
       for (let j = 0; j < gridNum; j++) {
         const square = document.createElement("div");
         square.classList.add("square");
+        square.addEventListener("mouseenter", updateColor);
         row.appendChild(square);
       }
     container.appendChild(row);  
   }
 }
 
+function updateColor (event){
+  event.target.classList.add("changecolor")
+}
 
-createGrid()
+
+function updateGrid() {
+  const userResponse = prompt()
+  
+}
+
+
+
+
+
+
+createGrid(16)
